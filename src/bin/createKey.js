ee561701c8315f06803d583e8daa6aa3d2a265af
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2019-09-16 17:53:35 +0800

    Add key to package.json to fix heroku error (#5)
    * feat(bin): add creating key cli
    
    * fix(root): fix heroku key in package.json

---
+++ src/bin/createKey.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+// @flow
+
+import fs from 'fs';
+import path from 'path';
+
+fs.writeFileSync(
+  path.resolve('key.json'),
+  JSON.stringify(
+    [
+      'type',
+      'project_id',
+      'private_key_id',
+      'private_key',
+      'client_email',
+      'client_id',
+      'auth_uri',
+      'token_uri',
+      'auth_provider_x509_cert_url',
+      'client_x509_cert_url',
+    ].reduce(
+      (result: {}, key: string) => ({
+        ...result,
+        [key]: process.env[key],
+      }),
+      {},
+    ),
+    null,
+    2,
+  ),
+  'utf8',
+);
